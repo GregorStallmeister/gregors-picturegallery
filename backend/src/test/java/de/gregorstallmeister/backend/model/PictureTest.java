403103cdf872b11bbaed8c285455719b73b11616
GregorStallmeister <post@gregor-stallmeister.de>
2025-03-25 16:09:05 +0100

    remove public modifier

--- backend/src/test/java/de/gregorstallmeister/backend/model/PictureTest.java
+++ backend/src/test/java/de/gregorstallmeister/backend/model/PictureTest.java
@@ -6,7 +6,7 @@ import java.time.ZonedDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class PictureTest {
+class PictureTest {
 
     @Test
     void constructorTest() {
