e024ec2fcd89368d807559a9d2773cee25f28b57
GregorStallmeister <post@gregor-stallmeister.de>
2025-03-27 13:09:17 +0100

    add PictureCard. add design for card. display one PictureCard.

--- frontend/src/components/DisplayPictures.tsx
+++ frontend/src/components/DisplayPictures.tsx
@@ -1,5 +1,10 @@
+import {PictureCard} from "./PictureCard.tsx";
+
 export function DisplayPictures () {
     return (
-        <div><h2>Fotos ansehen</h2></div>
+        <div>
+            <h2>Fotos ansehen</h2>
+            <PictureCard/>
+        </div>
     )
 }
---
+++ frontend/src/components/PictureCard.tsx
@@ -0,0 +1,7 @@
+export function PictureCard () {
+    return (
+        <div className="card">
+            &nbsp;PictureCard
+        </div>
+    )
+}
