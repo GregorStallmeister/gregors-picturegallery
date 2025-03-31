d26ccd36b8fb2013b4c3d65dbd3b23353ea81578
GregorStallmeister <post@gregor-stallmeister.de>
2025-03-31 08:54:55 +0200

    done little refactoring.

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -25,8 +25,12 @@ export function Home(props: Props) {
         }
     }
     console.log(randomString)
-    randomString = randomString.substring(0, 6)
 
+    while (randomString.length < 6) {
+        randomString = randomString + randomString
+    }
+
+    randomString = randomString.substring(0, 6)
     let indexBase: number = Number(randomString)
     indexBase = indexBase / 1000000
     console.log(indexBase)
