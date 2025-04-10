26a664096c0875293e43a901ee8b16ef1ab21dea
GregorStallmeister <post@gregor-stallmeister.de>
2025-04-10 12:27:16 +0200

    fix bug in delivery of content.

--- backend/src/main/java/de/gregorstallmeister/backend/controller/GlobalExceptionHandler.java
+++ backend/src/main/java/de/gregorstallmeister/backend/controller/GlobalExceptionHandler.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.time.Instant;
 import java.util.NoSuchElementException;
@@ -27,21 +26,6 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(CustomAuthenticationException.class)
     public ErrorMessage handleCustomAuthenticationException(@NotNull CustomAuthenticationException ex) {
         return new ErrorMessage("An error occurred: " + ex.getMessage(), Instant.now());
-    }
-
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(NoResourceFoundException.class)
-    public String handleNoResourceFoundException(@NotNull NoResourceFoundException ex) {
-        return "<html><head><title>Gregors Fotogalerie - Fehlerseite</title>" +
-                "<script>\n" +
-                "    window.location.href = \"" + getenv("APP_URL") + " \";\n" +
-                "  </script></head>" +
-                "<body style=\"background-color:#F6F2B4;\">Gregors Fotogalerie - schön, dass Sie da sind und herzlich willkommen!" +
-                "<br><br>Diese Seite ist leider nicht an allen Stellen kompatibel zum Neu-Laden per Browser-Button." +
-                "Sie werden in 5 Sekunden automatisch zur Startseite weitergeleitet. Sollte dies nicht geschehen, " +
-                "klicken Sie bitte <a href=\"" + getenv("APP_URL") + "\">hier</a>." +
-                "<br/>Vielen Dank und weiterhin viel Surf-Vergnügen!" +
-                "<br><br>Die Fehlermeldung war: " + ex.getMessage() + "</body></html>";
     }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
